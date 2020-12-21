08163f56eb2a9456e0175ea4cf31a61b275c1c5d
ZexHA <Enespp1@outlook.com>
2020-12-21 21:35:12 +0300

    Initial commit

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+-- Disable/enable sounds
+Config.Sounds = true
+Config.LoopSound = false
+Config.Volume = 1.0
+-- Min: 0.0 Max: 1.0
+
+-- Disable/enable Notifications
+Config.Notification = false
+Config.Strings = {seatbelt_on = 'Seatbelt: ~g~connected', seatbelt_off = 'Seatbelt: ~r~disconnected'}
+-- Change to your own translations.
+
+-- Disable/enable blinker image
+Config.Blinker = true
+
+-- Seatbelt button (docs.fivem.net/docs/game-references/controls)
+Config.Control = 311
+
+-- KM/H (must be have decimal value)
+Config.Speed = 200.0
+
+Config.AlarmOnlySpeed = true
+Config.AlarmSpeed = 10
+
